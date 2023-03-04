c3b2992cf40fa6aa39142670400538646fb06d2b
naveenao <107698978+naveenao@users.noreply.github.com>
2023-03-04 12:49:19 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "us-east-1" 
+       default     = "ap-south-1" 
 }
 
 variable "key_name" { 
-    description = " SSH keys to connect to ec2 instance" 
-    default     =  "mySep22Key" 
+    description = "SSH keys to connect to ec2 instance" 
+    default     =  "newwebkey" 
 }
 
 variable "instance_type" { 
